dac08cbf8b028b415272b8db10debe127c0f236a
Abdul Wasay <abdulwasaychoa@gmail.com>
2025-07-25 02:29:42 -0700

    Big Commit, add way to create the drawing board with specified input value (number of rows/cols) and color property to change the box color
    Features: display and remove grid on button click,
            change box-size and box-color, check function to see if number changed

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-console.log("Hello World")
+// Elements
+const gridContainer = document.querySelector('.drawpad')
+const boxesInput = document.querySelector('.controls .amount')
+const colorInput = document.querySelector('.controls .color')
+const submitBtn = document.querySelector('.submit')
+
+// Grid/Container Values
+const containerWidth = parseInt(getComputedStyle(gridContainer).getPropertyValue('width'))
+let numberOfBoxes = boxesInput.value;
+// Changeable Values
+let boxSize = containerWidth / numberOfBoxes;
+let paintColor = colorInput.value;
+
+function changeSize() {
+    numberOfBoxes = boxesInput.value;
+    boxSize = containerWidth / numberOfBoxes;
+}
+
+function changeColor() {
+    paintColor = colorInput.value;
+}
+
+function changeAllValues() {
+    changeSize()
+    changeColor()
+}
+
+function displayGrid() {
+    for(let i = 0; i < numberOfBoxes; i++) {
+        for(let j = 0; j < numberOfBoxes; j++) {
+            const box = document.createElement('div')
+            box.style.backgroundColor = paintColor
+            box.style.width = `${boxSize}px`
+            box.style.height = `${boxSize}px`
+            box.style.border = "2px solid black"
+            gridContainer.appendChild(box)
+        }
+    }
+}
+
+function removeGrid() {
+    if(gridContainer.hasChildNodes()) {
+        const allGridBoxes = Array.from(gridContainer.childNodes);
+        allGridBoxes.forEach((gridBox) => {
+            gridBox.remove()
+        })
+    }
+}
+
+function valuesHaveChanged() {
+    // Check if displayed boxes equal specified input value
+    const containerBoxes = gridContainer.childElementCount;
+    const totalInputBoxes = numberOfBoxes * numberOfBoxes // rows * columns
+    return (containerBoxes !== totalInputBoxes)
+}
+
+boxesInput.addEventListener('change', changeSize)
+colorInput.addEventListener('change', changeColor)
+
+submitBtn.addEventListener('click', () => {
+    // Display first grid when it has nothing
+    // Display all new grids when values have changed by removing previous grid
+    // If The Grid does not have same number of boxes to the inputs then remove and display
+    if(!gridContainer.hasChildNodes()) {
+        displayGrid()
+    } 
+
+    changeAllValues()
+
+    if (valuesHaveChanged()) {
+        removeGrid()
+        displayGrid()
+    }
+})
